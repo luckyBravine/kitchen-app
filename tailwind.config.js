be5fc1c2008569cbd0bf46b62f5fc88ad4ed7f6b
luckyBravine <bravinlude@gmail.com>
2023-08-10 19:41:43 +0300

    my third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "./public/index.html",
+  "./public/index.js"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
