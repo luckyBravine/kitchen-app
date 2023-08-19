c96ed223052fba117c29fa18078deffc7863dea1
luckyBravine <bravinlude@gmail.com>
2023-08-19 21:47:19 +0300

    my eigth and 2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,25 @@ import pasta from "../src/assets/pasta.png"
 import spagheti from "../src/assets/spagheti.png"
 import steak from "../src/assets/steak.png"
 import Circle from './Components/Circle';
+
+import { gsap } from "gsap";
+
 import './App.css';
-// import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
-  // const element1:HTMLElement | number | null = document.getElementById("circle1");
-  // const element2 = document.getElementById("circle2");
-  // const path = element1 - element2;
+  
+  let imageList = useRef(null);
+  let imageSlide = useRef(null);
+  let chefList = useRef(null);
 
-//   const pi = 3.14159;
-//   const circle1 = pi * 29.63 * 29.63
-//   const circle2 = pi * 22 * 22
-//   const path = circle1 - circle2
-//  console.log(path)
+  const [state, setState] = useState({
+    isActive1: true,
+    isActive2: false,
+    isActive3: false,
+    isActive4: false,
+  })
 
-  // const [change, setChange] = useState();
   const imageContent =[
     {
       firstName: 'Spagheti &',
@@ -63,30 +67,30 @@ function App() {
       dislikes: 0,
     },
     {
-      rating: 5.0,
+      rating: 5.3,
       icon: <AiFillStar />,
       name: 'Chef Ali',
       meal: 'Chicken Fillet',
       description: `When it comes to chicken fillet, Chef Ali is a true maestro. His dishes are a perfect symphony of flavors, textures, and presentation, leaving a lasting impression that's hard to forget`,
-      likes: 45,
+      likes: 68,
       dislikes: 0,
     },
     {
-      rating: 4.5,
+      rating: 5.5,
       icon: <AiFillStar />,
       name: 'Chef Kadzo',
       meal: 'Pasta Pak Adin',
       description: `Eating pasta from this chef is like taking a trip to Italy with each forkful. The way they capture the essence of Italian cuisine is truly remarkable.`,
-      likes: 45,
+      likes: 105,
       dislikes: 0,
     },
     {
-      rating: 4.5,
+      rating: 4.8,
       icon: <AiFillStar />,
       name: 'Chef Wafula',
       meal: 'Steak Fillet',
       description: `A culinary virtuoso with a fillet as their masterpiece. This chef's attention to detail and dedication to perfection are truly remarkable.`,
-      likes: 45,
+      likes: 54,
       dislikes: 0,
     }
   ]
@@ -96,7 +100,7 @@ function App() {
   return (
    <section className="bg-slate-100 relative w-full">
     <div className="grid grid-cols-2 w-[100vw] justify-center mx-auto gap-1">
-      <div className='flex flex-col relative bg-purple-800'>
+      <div className='flex flex-col relative bg-purple-800' ref={el => {imageList = el}}>
         {/* <div id="circle1" className="top-[-118vh] ml-[-14rem] p-4 relative h-[70rem] w-[70rem] bg-gradient-to-r from-purple-400 to-pink-400 rounded-full flex justify-center items-center">
           <div id="circle2" className="bg-slate-100 rounded-full h-[52rem] w-[52rem]">
               <div className='absolute flex justify-center items-center mb-0 ml-[8rem] top-[120vh]'>
@@ -123,7 +127,7 @@ function App() {
         <Circle images={imageContent} />
         <div className='flex items-center mx-auto relative'>
           <AiOutlineLeft className="text-2xl font-black"/>
-          <div className='flex mx-auto px-2'>
+          <div className='flex mx-auto px-2' ref={el => {imageSlide = el}}>
             <div className='flex-col hover:bg-stone-200 bg-slate-200 justify-center w-[100px] items-center p-1 mx-6 rounded-md'>
               <img src={imageContent[0].image} alt="" className='w-18 h-20' />
               <h4 className='font-normal font-roboto text-md mx-auto text-center '>
@@ -169,29 +173,117 @@ function App() {
             <h5 className='text-md mr-4 font-semibold'>Overview</h5>
             <h5 className='text-md text-gray-700 ml-4'>Ingredients</h5>
           </div>
-          <div className='w-12 mt-6 ml-6 pl-4 h-16 flex flex-col justify-center bg-green-800 rounded-lg'>
-            <h2 className='text-4xl font-black'>
-              {chefContent[0].rating}
-            </h2>
-            <AiFillStar />
-          </div>
-          <div className='flex flex-col mx-auto justify-center px-6 mt-4'>
-            <h3 className='text-xl font-bold'>
-              {chefContent[0].name}
-            </h3>
-            <span className='text-xs py-1 text-slate-400'>
-              {chefContent[0].meal}
-            </span>
-            <p className='text-base font-roboto font-normal mt-1 leading-tight'>
-              {chefContent[0].description}
-            </p>
-            <div className='flex items-center mt-4'>
-              <div className='p-2 bg-slate-300 mr-3 rounded-lg flex items-center'>
-                <VscThumbsup className="mr-2" />
-                <span> {chefContent[0].likes} likes</span>
+          <div className='flex relative w-[20rem] mx-auto'>
+            <div className='absolute' ref={el => {chefList = el}}>
+              <div className='w-12 mt-6 ml-6 pl-4 h-16 flex flex-col justify-center bg-green-800 rounded-lg'>
+                <h2 className='text-4xl font-black'>
+                  {chefContent[0].rating}
+                </h2>
+                <AiFillStar />
               </div>
-              <div className='p-2 bg-slate-300 ml-3 rounded-lg flex items-center'>
-                <VscThumbsdown className="mr-2" />
+              <div className='flex flex-col mx-auto justify-center px-6 mt-4'>
+                <h3 className='text-xl font-bold'>
+                  {chefContent[0].name}
+                </h3>
+                <span className='text-xs py-1 text-slate-400'>
+                  {chefContent[0].meal}
+                </span>
+                <p className='text-base font-roboto font-normal mt-1 leading-tight'>
+                  {chefContent[0].description}
+                </p>
+                <div className='flex items-center mt-4'>
+                  <div className='p-2 bg-slate-300 mr-3 rounded-lg flex items-center'>
+                    <VscThumbsup className="mr-2" />
+                    <span> {chefContent[0].likes} likes</span>
+                  </div>
+                  <div className='p-2 bg-slate-300 ml-3 rounded-lg flex items-center'>
+                    <VscThumbsdown className="mr-2" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='absolute'>
+              <div className='w-12 mt-6 ml-6 pl-4 h-16 flex flex-col justify-center bg-green-800 rounded-lg'>
+                <h2 className='text-4xl font-black'>
+                  {chefContent[1].rating}
+                </h2>
+                <AiFillStar />
+              </div>
+              <div className='flex flex-col mx-auto justify-center px-6 mt-4'>
+                <h3 className='text-xl font-bold'>
+                  {chefContent[1].name}
+                </h3>
+                <span className='text-xs py-1 text-slate-400'>
+                  {chefContent[1].meal}
+                </span>
+                <p className='text-base font-roboto font-normal mt-1 leading-tight'>
+                  {chefContent[1].description}
+                </p>
+                <div className='flex items-center mt-4'>
+                  <div className='p-2 bg-slate-300 mr-3 rounded-lg flex items-center'>
+                    <VscThumbsup className="mr-2" />
+                    <span> {chefContent[1].likes} likes</span>
+                  </div>
+                  <div className='p-2 bg-slate-300 ml-3 rounded-lg flex items-center'>
+                    <VscThumbsdown className="mr-2" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='absolute'>
+              <div className='w-12 mt-6 ml-6 pl-4 h-16 flex flex-col justify-center bg-green-800 rounded-lg'>
+                <h2 className='text-4xl font-black'>
+                  {chefContent[2].rating}
+                </h2>
+                <AiFillStar />
+              </div>
+              <div className='flex flex-col mx-auto justify-center px-6 mt-4'>
+                <h3 className='text-xl font-bold'>
+                  {chefContent[2].name}
+                </h3>
+                <span className='text-xs py-1 text-slate-400'>
+                  {chefContent[2].meal}
+                </span>
+                <p className='text-base font-roboto font-normal mt-1 leading-tight'>
+                  {chefContent[2].description}
+                </p>
+                <div className='flex items-center mt-4'>
+                  <div className='p-2 bg-slate-300 mr-3 rounded-lg flex items-center'>
+                    <VscThumbsup className="mr-2" />
+                    <span> {chefContent[2].likes} likes</span>
+                  </div>
+                  <div className='p-2 bg-slate-300 ml-3 rounded-lg flex items-center'>
+                    <VscThumbsdown className="mr-2" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='absolute'>
+              <div className='w-12 mt-6 ml-6 pl-4 h-16 flex flex-col justify-center bg-green-800 rounded-lg'>
+                <h2 className='text-4xl font-black'>
+                  {chefContent[3].rating}
+                </h2>
+                <AiFillStar />
+              </div>
+              <div className='flex flex-col mx-auto justify-center px-6 mt-4'>
+                <h3 className='text-xl font-bold'>
+                  {chefContent[3].name}
+                </h3>
+                <span className='text-xs py-1 text-slate-400'>
+                  {chefContent[3].meal}
+                </span>
+                <p className='text-base font-roboto font-normal mt-1 leading-tight'>
+                  {chefContent[3].description}
+                </p>
+                <div className='flex items-center mt-4'>
+                  <div className='p-2 bg-slate-300 mr-3 rounded-lg flex items-center'>
+                    <VscThumbsup className="mr-2" />
+                    <span> {chefContent[3].likes} likes</span>
+                  </div>
+                  <div className='p-2 bg-slate-300 ml-3 rounded-lg flex items-center'>
+                    <VscThumbsdown className="mr-2" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
